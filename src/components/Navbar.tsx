b87a54ad69b09c0741f24dc4310de8cb86fa5686
이진영 <swlsdud1005@naver.com>
2023-07-18 22:58:41 +0900

    navbar 기본골격, 스타일링 1차 적용

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,26 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { cssType } from "../App";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  const navCss: cssType = `w-[25%] h-full flex justify-center items-center font-semibold text-lg text-white bg-lime-500 hover:bg-lime-600`;
+  const newQuiz = () => {
+    window.location.replace("/quiz");
+  };
+  return (
+    <section className="flex w-full h-[10%] rounded-t-2xl">
+      <NavLink className={`${navCss} rounded-tl-2xl`} to="/wordbook">
+        단어장
+      </NavLink>
+      <NavLink className={navCss} to="/bookmark">
+        북마크
+      </NavLink>
+      <NavLink className={navCss} to="/quiz" onClick={newQuiz}>
+        퀴즈
+      </NavLink>
+      <NavLink className={`${navCss} rounded-tr-2xl`} to="/myPage">
+        마이페이지
+      </NavLink>
+    </section>
+  );
 }
