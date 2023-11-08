4d32787fabfd11d76c16bd91c8b1e7dd0e4d1e0d
이진영 <swlsdud1005@naver.com>
2023-11-08 10:36:30 +0900

    코드 1차 정리 - 배포 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import App from "./App";
-import { HashRouter, BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { AuthContextProvider } from "./utils/AuthContext";
 
@@ -10,11 +10,11 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <QueryClientProvider client={queryClient}>
       <AuthContextProvider>
         <App />
       </AuthContextProvider>
     </QueryClientProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
